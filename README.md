986aa5e4ae6d3b03e7520b6f78740d832bc15327
Steven Setiawan Salim <sstevensetiawan@gmail.com>
2020-08-04 16:25:53 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+cara installasi clone project ini lalu jalankan perintah
+
+## npm install
+
+tunggu sampe selesai lalu setelah itu jalankan 
+
+### npm start
+
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import DaftarBuah from './tugas11/DaftarBuah';
+
+function App() {
+  return (
+    <div>
+      <DaftarBuah />
+    </div>
+  );
+}
+
+export default App;
